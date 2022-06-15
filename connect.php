dbef7327ebc8fad1f9f1260c51aa0297919efe95
Hisrahel <olayemisrael5@gmail.com>
2022-06-15 06:11:00 -0700

    ghg

--- connect.php
+++ connect.php
@@ -51,8 +51,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LOGIN SUCCESSFUL</title>
-    <link rel="icon" href="image/logo.png">
+    <title>Login Successful</title>
+    <link rel="icon" href="image/unnamed.png">
   <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.css">
   <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="cone.css">
